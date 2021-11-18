a31cd331b9b12378e60283de3f0ed2fb2871884e
vijay pratap <vijaypratap@vijays-MacBook-Pro.local>
2021-11-18 14:34:04 +0530

    added invalid data exception in exceptiom package

---
+++ src/com/lti/exception/InvalidDataAccessException.java
@@ -0,0 +1,9 @@
+package com.lti.exception;
+
+public class InvalidDataAccessException extends RuntimeException {
+
+    public InvalidDataAccessException(String message) {
+        // TODO Auto-generated constructor stub
+        super(message);
+    }
+}
